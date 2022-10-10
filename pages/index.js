a613d9f5ce00bcd58b9d5c6bfda08729e33ea9be
Hamzah Fansyuri <53176833+hf-lab@users.noreply.github.com>
2022-10-10 22:38:17 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,42 +14,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to IELTS Book Repository!" />
-        <p><a href="https://www.ieltsclue.com/" target="_blank">Preparing IELTS</a> exam can at times appear daunting, but it need not be. You will succeed if you have patience, a willingness to learn, and determination. Additionally, preparatory texts are required for success. These books will assist you in better comprehending the exam subject, achieving a high mark, and standing out from the throng.</p>
-<p>With so many options accessible, we have compiled a list of the five best IELTS study guides. While the list is not in any specific sequence, one item may be more suitable for you than the rest.</p>
-<h3>Best Books for IELTS Preparation</h3>
-<h4><a href="https://www.ieltsclue.com/2022/07/official-ielts-practice-materials.html" target="_blank">1. The Official Cambridge Guide to IELTS</a>&nbsp;</h4>
-<div>This book is ideal for both the IELTS General Training and Academic Reading tests. If you are searching for a book, this is the one you should select first. The book contains numerous tips, suggestions, and hints for passing the exam. These tips come from from those who completed the course with outstanding scores. Nothing is more ideal than a personal relationship. <a href="https://www.ieltsclue.com/2022/09/the-official-cambridge-guide-to-ielts.html" target="_blank">The Official Cambridge Guide To IELTS PDF + Audio</a></div>
-<h4><a href="https://www.ieltsclue.com/2022/10/barrons-ielts-superpack-pdf-free.html" target="_blank">2. Barron&rsquo;s IELTS &nbsp;Superpack&nbsp;</a></h4>
-<div>It is one of those books that is always in the buzz that you can easily acquire for yourself from an online retailer such as Amazon. The fact that the book is so chock-full of useful advice and strategies is a major benefit, both for the book itself and for individuals who are working toward mastering English language skills.<br>To begin, let&apos;s get this out of the way: it&apos;s a fantastic book, both from a professional and an academic one. Because it contains everything that can assist you in achieving a decent score on the IELTS general test, this comprehensive IELTS preparation pack is appropriate for that test.</div>
-<h4><a href="https://www.ieltsclue.com/search/label/E-Book" target="_blank">3. Cambridge IELTS Academic and General Student&rsquo;s Book</a></h4>
-<div>The Cambridge IELTS Academic Student&apos;s Book is well-known for its credibility and can provide a general overview of the questions that will be included in the exam. This well-known series focuses mainly on the Academic portion of the IELTS exam. It includes contemporary reading material, which is accompanied by four practice exams and their respective solutions.</div>
-<div>
-    <ul>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-17-academic-and-general.html" target="_blank">Cambridge IELTS 17 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-16-academic-and-general.html" target="_blank">Cambridge IELTS 16 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-15-academic-and-general.html" target="_blank">Cambridge IELTS 15 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-14-academic-and-general.html" target="_blank">Cambridge IELTS 14 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-13-academic-and-general.html">Cambridge IELTS 13 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-12-academic-and-general.html" target="_blank">Cambridge IELTS 12 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-11-academic-and-general.html" target="_blank">Cambridge IELTS 11 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-10-academic-and-general.html" target="_blank">Cambridge IELTS 10 Book</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/07/cambridge-ielts-9-academic-and-general.html" target="_blank">Cambridge IELTS 9 Book</a></li>
-    </ul>
-    <h4><a href="https://www.ieltsclue.com/2022/08/ielts-makkar-ebook-pdf.html" target="_blank">4. Makkar IELTS</a></h4>
-</div>
-<div>The Makkar IELTS Guides are in the lead thanks to their six distinct volumes, four of which are geared toward academic preparation and the remaining two toward general training practice. Every Makkar IELTS book provides you with a variety of lessons, skills, and practice tests that can help you prepare for the IELTS examination. These books are developed specifically for beginning to average readers. <a href="https://www.ieltsclue.com/2022/09/makkar-ielts-speaking-final-version-sep-dec-2022.html" target="_blank">Makkar IELTS Speaking final version Sep-Dec 2022 PDF</a><br>Through the course of reading this text, we will lead you to the Makkar IELTS Guides. We will provide in-depth information about each book, including its title, author, contents, price, the advantages and disadvantages of selecting the Makkar book series, and a few user ratings of the overall collection as a whole.</div>
-<h3><a href="https://www.ieltsclue.com/" target="_blank">IELTS Book Preparation</a></h3>
-<div>
-    <ol>
-        <li><a href="https://www.ieltsclue.com/2022/08/ielts-vocabulary-masterclass-8-5-by-marc-roche.html" target="_blank">IELTS Vocabulary Masterclass 8.5 Book 1-3 by Marc Roche PDF</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/09/ielts-recent-actual-tests-may-august-2022.html">IELTS Recent Actual Tests May-August 2022 Book with Suggested Answers</a></li>
-        <li><a href="https://www.ieltsclue.com/2022/09/improve-your-skills-writing-for.html" target="_blank">Improve your Skills Writing for Advanced Ebook PDF Free</a></li>
-    </ol>Finally, we just have one piece of advice: begin, begin your preparations as soon as possible, and do so with confidence, not self-doubt. Which is why referring to good books is essential, and books are great since they never go out of style, even in the presence of sophisticated technology. The secret to exam preparation is awareness. The presence of an excellent book can provide you with enormous power.<br>The Internet is wonderful, but it has many unavoidable distractions, including something as serious as IELTS. We hope the books we suggested help you with your preparations. We&apos;ll keep publishing on study abroad and other topics. Maintain communication. Sign up for University HUB now and check out your best options if you&apos;re seeking for a better approach to study abroad that isn&apos;t complicated, demanding, or time-consuming.
-</div>
-<div><br></div>
-<div>Tags:</div>
-<div>#ielts book #ielts book a test #ielts book for preparation #prepare for ielts book #idp ielts book test #cambridge ielts book #best ielts book #ielts book test #ielts book pdf #british council ielts book #ielts book date #british council ielts book free download pdf</div>
-      </main>
+       </main>
 
       <Footer />
     </div>
